81de46d55bda682ffb2c2f2835f01ff9750099ea
Bernd Brägelmann <bernd@braegelmann.de>
2012-08-01 23:16:20 +0200

    form

---
+++ src/main/java/de/braegel/diagnozo/pages/CreateDiagnose.java
@@ -0,0 +1,10 @@
+package de.braegel.diagnozo.pages;
+
+import org.apache.tapestry5.annotations.Property;
+
+import de.braegel.diagnozo.entities.Diagnosis;
+
+public class CreateDiagnose {
+	 @Property
+	  private Diagnosis diagnosis;
+}
